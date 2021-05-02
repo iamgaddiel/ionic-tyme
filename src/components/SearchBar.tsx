41a6a6083860ecb91a9adb5912b6f205cd55147d
Gaddiel <ighotagaddiel98@gmail.com>
2021-05-02 02:59:30 +0100

    refactored searchbar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import {IonText, IonItem, IonIcon, IonButton, IonInput} from '@ionic/react';
-import {search} from 'ionicons/icons';
+import React, { useState } from 'react';
+import { IonSearchbar, IonText } from '@ionic/react';
 
 const Search = () => {
+    const [search, setSearch] = useState<string>('');
+
+    const handleSearch = (e: any) => {
+        let searchValue = e.detail.value!;
+        setSearch(searchValue);
+    }
     return (
         <section className="search-prescription ion-margin-bottom">
-                <form action="" method="post">
-                    <IonText color="medium">Search for prescription</IonText>
-                    <IonItem id="search-item" className="ion-margin-top">
-                        <IonButton slot="start" type="submit" color="default" id="search-btn">
-                            <IonIcon icon={search} color="dark"></IonIcon>
-                        </IonButton>
-                        <IonInput
-                            type="search"
-                            placeholder="Search..."
-                            autocomplete="on"
-                            autocorrect="on" 
-                        />
-                    </IonItem>
-                </form>
-            </section>
+            <IonText>Search for prescription</IonText>
+            <IonSearchbar
+                value={search}
+                animated
+                type="search"
+                showCancelButton="never"
+                showClearButton="always"
+                autoCorrect="on"
+                spellCheck={true}
+                onIonChange={handleSearch} />
+        </section>
     )
 }
 
